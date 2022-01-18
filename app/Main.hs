1ac209c77f3ee94feeb198421167b973cbb97bcd
Joel E Carlson <joel_carlson@optum.com>
2022-01-18 10:30:36 -0600

    HUD

--- app/Main.hs
+++ app/Main.hs
@@ -20,7 +20,7 @@ width, height :: Int
 
 main :: IO ()
 main = do
-  world <- newIORef $ mkWorld (5, 5) (width, height) 10 10
+  world <- newIORef $ mkWorld (5, 5) (width, height) 80 50
   U.withSDL $ U.withSDLImage $ do
     U.withWindow "Arrow" (width, height) $ \w ->
       U.withRenderer w $ \r -> do

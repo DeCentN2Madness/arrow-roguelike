2ea98407a119ba3dc90f97daaf1073533415018c
Joel E Carlson <joel_carlson@optum.com>
2022-02-10 10:19:19 -0600

    Game.AI

--- src/Game/AI.hs
+++ src/Game/AI.hs
@@ -5,13 +5,44 @@ Game.AI.hs
 Author: "Joel E Carlson" <joel.elmer.carlson@gmail.com>
 
 -}
-module Game.AI (pathFinder) where
+module Game.AI (aiAction, pathFinder) where
 
 import Engine.Arrow.Data (World(..))
 import Data.List
+import qualified Game.Combat as GC
 import qualified Game.Entity as GE
 import Game.Kind.Entity (EntityKind(..))
 
+data AI = AI
+  | Attack
+  | Move
+  | Wait
+  deriving (Show, Eq)
+
+-- | aiAction
+-- handle actions based on goal
+-- TODO goal is affected by hitPoint, patrol, status; npc, so on...
+aiAction :: [(Int, EntityKind)] -> World -> World
+aiAction [] w = w
+aiAction ((mx, mEntity):xs) w = if mx == 0 || not (block mEntity)
+  then aiAction xs w
+  else let
+  (_, pPos) = GE.getPlayer (entityT w)
+  distanceList = [ (d, xy) | xy <- moveT mEntity,
+                  let d = distance pPos xy ]
+  actionList   = [ x | (d, _) <- sort distanceList,
+                        let x = if d==1 then Attack else Wait ]
+  action = if not (null actionList)
+    then head actionList
+    else Wait
+  newWorld = case action of
+    Attack -> GC.mkCombat mx 0 w
+    Move   -> w
+    Wait   -> w
+    _      -> w
+  in aiAction xs newWorld
+
+-- | distance
 distance :: (Int, Int) -> (Int, Int) -> Double
 distance (x1, y1) (x2, y2) = let
   distX = fromIntegral $ (x1 - x2) * (x1 - x2)
@@ -27,7 +58,7 @@ distance (x1, y1) (x2, y2) = let
 -- TODO goal is affected by hitPoint, patrol, status; npc, so on...
 pathFinder :: [(Int, EntityKind)] -> World -> World
 pathFinder [] w = w
-pathFinder ((mx, mEntity):xs) w = if mx == 0
+pathFinder ((mx, mEntity):xs) w = if mx == 0 || not (block mEntity)
   then pathFinder xs w
   else let
   coordF :: [(Int, Int)] -> [(Int, Int)]

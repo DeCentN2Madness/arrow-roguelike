581cfbdbf97be69cbafea00ecf7d5814b0cf7a42
Joel E Carlson <joel.elmer.carlson@gmail.com>
2022-06-06 14:02:08 -0500

    0.2.18.0

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,7 @@ Author: "Joel E Carlson" <joel.elmer.carlson@gmail.com>
 module Main (main) where
 
 import Data.IORef
+import Control.Concurrent (threadDelay)
 import Control.Monad (forM_)
 import Control.Monad.Extra (unless)
 import qualified SDL
@@ -54,5 +55,5 @@ mainLoop world render ts = do
     modifyIORef world (GA.applyIntent i)
     d <- readIORef world
     EDU.draw render ts d
-  SDL.delay 10
+  threadDelay 10000
   unless (gameState q == GameStop) $ mainLoop world render ts

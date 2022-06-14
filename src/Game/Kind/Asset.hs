ed2701301daef8bff1f79923cc666af9c07fffd0
Joel E Carlson <joel.elmer.carlson@gmail.com>
2022-06-14 13:18:25 -0500

    0.2.18.0

--- src/Game/Kind/Asset.hs
+++ src/Game/Kind/Asset.hs
@@ -49,15 +49,14 @@ mkAssetMap = let
        , mkItem "melee/Dagger"          "|:1d4:1:Finesse,Light" pos
        , mkItem "melee/Greatclub"       "|:1d8:10:Heavy"        pos
        , mkItem "melee/Handaxe"         "|:1d6:2:Light"         pos
+       , mkItem "melee/Light Hammer"    "|:1d4:2:"              pos
        , mkItem "melee/Mace"            "|:1d6:4:"              pos
-       , mkItem "melee/Pick"            "|:1d8:6:Digger"        pos
        , mkItem "melee/Quarterstaff"    "|:1d6:4:"              pos
        , mkItem "melee/Sickle"          "|:1d4:2:Light"         pos
        , mkItem "melee/Spear"           "|:1d6:3:"              pos
-       , mkItem "melee/Whip"            "|:1d4:2:Finesse"       pos
        -- melee martial
        , mkItem "melee/Battleaxe"       "|:1d8:4:"              pos
-       , mkItem "melee/Flail"           "|:1d8:3:"              pos
+       , mkItem "melee/Flail"           "|:1d8:2:"              pos
        , mkItem "melee/Glaive"          "|:1d10:6:Heavy"        pos
        , mkItem "melee/Greataxe"        "|:1d12:7:Heavy"        pos
        , mkItem "melee/Halberd"         "|:1d10:6:Heavy"        pos
@@ -87,15 +86,14 @@ mkAssetMap = let
        , mkItem "shoot/Crossbow, light" "}:1d8:5:Ammo"         pos
        , mkItem "shoot/Crossbow, hand"  "}:1d6:3:Ammo,Light"   pos
        , mkItem "shoot/Crossbow, heavy" "}:1d10:18:Ammo,Heavy" pos
-       , mkItem "shoot/Dart"            "}:1d4:1:Ammo"         pos
-       , mkItem "shoot/Javelin"         "|:1d6:2:Ammo"         pos
-       , mkItem "shoot/Longbow"         "}:1d8:2:Ammo"         pos
+       , mkItem "shoot/Dart"            "}:1d4:1:Thrown"       pos
+       , mkItem "shoot/Javelin"         "|:1d6:2:Thrown"       pos
+       , mkItem "shoot/Longbow"         "}:1d8:2:Ammo,Heavy"   pos
        , mkItem "shoot/Sling"           "}:1d4:0:Ammo"         pos
        , mkItem "shoot/Shortbow"        "}:1d6:2:Ammo"         pos
-       , mkItem "shoot/Throwing Knife"  "|:1d4:1:Ammo"         pos
-       , mkItem "shoot/Throwing Axe"    "|:1d6:2:Ammo"         pos
+       , mkItem "shoot/Throwing Knife"  "|:1d4:1:Thrown"       pos
+       , mkItem "shoot/Throwing Axe"    "|:1d6:2:Thrown"       pos
        -- shoot Elvish
-       , mkItem "shoot/Elvish Bow"      "}:2d4:2:Ammo" pos
        , mkItem "shoot/Elvish Longbow"  "}:2d5:2:Ammo" pos
        , mkItem "shoot/Elvish Shortbow" "}:2d3:2:Ammo" pos
        -- Armor
@@ -123,7 +121,7 @@ mkAssetMap = let
        -- Feet
        , mkItem "feet/Boots"              "]:1:1:" pos
        , mkItem "feet/Boots of Elvenkind" "]:100:1:DEX,COLD" pos
-       , mkItem "feet/Boots of Speed"     "]:100:1:DEX" pos
+       , mkItem "feet/Boots of Speed"     "]:100:1:DEX,AC" pos
        , mkItem "feet/Dwarven Boots"      "]:100:1:STR,COLD" pos
        , mkItem "feet/Horseman Boots"     "]:100:1:CON,FIRE" pos
        , mkItem "feet/Mail Chausses"      "]:100:1:AC" pos
@@ -134,14 +132,14 @@ mkAssetMap = let
        , mkItem "hands/Gloves of Thieves"   "]:100:1:DEX,INT" pos
        , mkItem "hands/Bracers of Warrior"  "]:100:1:STR,ATTACK" pos
        , mkItem "hands/Bracers of Hunter"   "]:100:1:DEX,SHOOT" pos
-       , mkItem "hands/Gauntlets of Ogre"   "]:100:1:STR" pos
+       , mkItem "hands/Gauntlets of Ogre"   "]:100:1:STR,CON" pos
        , mkItem "hands/Gauntlets of Power"  "]:100:1:INT,CON" pos
        , mkItem "hands/Gauntlets of Frost"  "]:100:1:COLD" pos
        , mkItem "hands/Gauntlets of Flames" "]:100:1:FIRE" pos
        -- Rings
        , mkItem "jewelry/Ring"            "=:10:1:" pos
        , mkItem "jewelry/Ring of Dwarves" "=:100:1:CON" pos
-       , mkItem "jewelry/Ring of Elves"   "=:100:1:DEX,INT" pos
+       , mkItem "jewelry/Ring of Elves"   "=:100:1:INT,WIS" pos
        , mkItem "jewelry/Ring of Magi"    "=:100:1:INT" pos
        , mkItem "jewelry/Ring of Priest"  "=:100:1:WIS" pos
        , mkItem "jewelry/Ring of Thief"   "=:100:1:DEX" pos
@@ -160,7 +158,7 @@ mkAssetMap = let
        , mkItem "cloak/Cloak"                "(:1:1:" pos
        , mkItem "cloak/Cloak of Iron Throne" "(:100:1:CON,SEARCH" pos
        , mkItem "cloak/Cloak of Elvenkind"   "(:100:1:DEX,SEARCH" pos
-       , mkItem "cloak/Cloak of Elements"    "(:100:1:INT,FIRE,COLD" pos
+       , mkItem "cloak/Cloak of Elements"    "(:100:1:FIRE,COLD" pos
        , mkItem "cloak/Cloak of Protection"  "(:100:1:AC" pos
        , mkItem "cloak/Cloak of Dragon"      "(:100:1:CON,FIRE" pos
        , mkItem "cloak/Cloak of Hunter"      "(:100:1:DEX,SHOOT" pos
